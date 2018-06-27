5e88c30574364502b65f3c2d3af3dfa8740c0ae7
Asavari Tayal <asavaritayal@Asavaris-MacBook-Pro.local>
2018-06-27 15:17:26 -0700

    flask framework check

--- 3.6.5/entrypoint.py
+++ 3.6.5/entrypoint.py
@@ -1,6 +1,8 @@
 import subprocess
 import os
 
+HOME_SITE="/home/site/wwwroot"
+
 # Temporary patch. Won't be required once antenv is built in the kudu image.
 os.environ["PYTHONPATH"] = "/home/site/wwwroot/antenv/lib/python3.6/site-packages"
 
@@ -9,6 +11,25 @@ def subprocess_cmd(command):
     proc_stdout = process.communicate()[0].strip()
     print (proc_stdout.decode("utf-8"))
 
+## Check for custom gunicorn config
+def custom_check():
+    return None
+
+## Django check
+# If manage.py is present in the current or a subdirectory, find wsgi object and identify as Django. 
+def check_django():
+    return None
+
+## Flask check
+# If application.py is provided or only a single module is present, identify as Flask.
+def check_flask():
+    if os.path.exists(HOME_SITE + "/application.py"):
+        return 'application'
+    else:
+        return None
+
+app_module = check_flask()
+
 subprocess_cmd('python --version; pip --version;'
-                'GUNICORN_CMD_ARGS="--bind=0.0.0.0" gunicorn application:app'
+                'GUNICORN_CMD_ARGS="--bind=0.0.0.0" gunicorn ' + app_module + ':app'
                )
