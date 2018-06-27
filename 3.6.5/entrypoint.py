869dcf2a6c45807be15f3cb6b819e36ca69146f6
Asavari Tayal <asavaritayal@Asavaris-MacBook-Pro.local>
2018-06-27 16:37:35 -0700

    Adding kudu placeholder script

--- 3.6.5/entrypoint.py
+++ 3.6.5/entrypoint.py
@@ -3,33 +3,31 @@ import os
 
 HOME_SITE="/home/site/wwwroot"
 
-# Temporary patch. Won't be required once antenv is built in the kudu image.
-os.environ["PYTHONPATH"] = "/home/site/wwwroot/antenv/lib/python3.6/site-packages"
+# Temporary patch. Remove when Kudu script is available.
+os.environ["PYTHONPATH"] = HOME_SITE + "/antenv/lib/python3.6/site-packages"
 
 def subprocess_cmd(command):
     process = subprocess.Popen(command,stdout=subprocess.PIPE, shell=True)
     proc_stdout = process.communicate()[0].strip()
     print (proc_stdout.decode("utf-8"))
 
-## Check for custom gunicorn config
+## Check for custom startup command
 def custom_check():
     return None
 
-## Django check
-# If manage.py is present in the current or a subdirectory, find wsgi object and identify as Django. 
+## Django check : If manage.py is present in the project, find wsgi object and identify as Django. 
 def check_django():
     return None
 
-## Flask check
-# If application.py is provided or only a single module is present, identify as Flask.
+## Flask check : If application.py is provided or only a single module is present, identify as Flask.
 def check_flask():
     if os.path.exists(HOME_SITE + "/application.py"):
-        return 'application'
+        return 'application:app'
     else:
         return None
 
 app_module = check_flask()
 
 subprocess_cmd('python --version; pip --version;'
-                'GUNICORN_CMD_ARGS="--bind=0.0.0.0" gunicorn ' + app_module + ':app'
+                'GUNICORN_CMD_ARGS="--bind=0.0.0.0" gunicorn ' + app_module
                )
